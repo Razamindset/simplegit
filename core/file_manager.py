9b61d3786e5d848fd0c036a951f25c820151a1a6
Razamindset <razamindset.official@gmail.com>
2026-06-01 15:23:27 +0500

    update: add file manager

---
+++ core/file_manager.py
@@ -0,0 +1,15 @@
+class FileManager:
+    def __init__(self):
+        pass
+    
+    def copy_project_files(self, project_dir, files_path):
+        pass
+
+    
+    def restore_files(aelf):
+        pass
+
+    def delete_working_files(aelf):
+        pass
+
+
